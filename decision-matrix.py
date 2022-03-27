c4426a10e57929afc93eb3083e712f738d48e8e0
Laurel Mossor <laurel.mossor@gmail.com>
2022-03-26 18:26:20 -0700

    Stuck on identify_criteria

--- decision-matrix.py
+++ decision-matrix.py
@@ -20,6 +20,18 @@ class Decision:
 
 
 ##################### PROGRAM FUNCTIONS ##########################
+def yes_TRUE_no_FALSE() ->bool:
+    while True:
+        user_choice = input("> ")
+
+        if user_choice.upper() == "Y":
+            print("Great, let's move on.")
+            return True
+        if user_choice.upper() == "N":
+            return False
+        else:
+            print("Please select Y or N.")
+
 def greet_user():
     """Greet the user and assign name"""
     print("Hello and welcome to the Decision Matrix. Let's break down a difficult choice in some parts. \nTo start, what is your name?")
@@ -42,26 +54,43 @@ def identify_decision(user_name):
     while True:
         user_decision = input("I need to... ")
         print(f"You need to {user_decision}, is that correct? [Y/N]")
-        choice = input("> ").upper()
-        if choice == "Y":
-            print("Great, let's move on.")
+        
+        if yes_TRUE_no_FALSE():
             break
-        if choice == "N":
-            continue
-        else:
-            print("Please select Y or N.")
+
     
     x.add_decision(user_decision)
 
 # While loop to identify all criteria
 def identify_criteria():
     print("""Next up, we're going to identify what is most important to take into consideration when making this choice.
-    > If you are picking out a new car, you might assign your criteria to be 
-    size, cost, color, and gas mileage.
-    > If you're choosing which school to go to, you might choose the following criteria; 
-    cost per credit hour, yearly tuition, location, and funding oppotunities.""")
 
+    If you are picking out a new car, you might assign your criteria to be 
+    > size, cost, color, and gas mileage.
 
+    If you're choosing which school to go to, you might choose the following criteria; 
+    > cost per credit hour, yearly tuition, location, and funding oppotunities.
+
+Let's assign the first criterion.""")
+    print()
+    
+    while True:
+        print("What is an important factor in your decision?")
+        criterion = input("> ")
+        print(f"You put '{criterion}'. Is that correct? [Y/N]")
+        yes_TRUE_no_FALSE
+        if yes_TRUE_no_FALSE():
+            x.add_criteria(criterion)
+            print(f"Your current criteria: {x.criteria}")
+        print("Would you like to add another criteria?")
+        if yes_TRUE_no_FALSE:
+            continue
+        else:
+            print(f"Here' your current list of important criteria: {x.criteria}. \n Ready to move on?")
+        if yes_TRUE_no_FALSE:
+            print("Great, on to the next thing.")
+            break
+        
 ##################### MAIN ##########################
 
 x = Decision()
