4b707364458891af38afe9945c3b8909f4ed5925
Laurel Mossor <laurel.mossor@gmail.com>
2022-03-29 21:54:31 -0700

    Figured out how to display populated data on same page after submit

--- practice-site.py
+++ practice-site.py
@@ -13,7 +13,12 @@ def home():
 def getting_started():
     """Starts the decision matrix"""
 
-    return render_template("get-started.html")
+    current_user = request.args.get("current-user")
+    user_decision = request.args.get("user-decision")
+    populated_values = [request.args.get("current-user"), request.args.get("user-decision")]
+
+    return render_template("get-started.html", name=current_user, 
+        decision=user_decision, values=populated_values)
 
 
 @app.route("/identify-criteria")
