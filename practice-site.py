8f5e3f843bf584f298032568e07b17fba1ea9fac
Laurel Mossor <laurel.mossor@gmail.com>
2022-03-29 20:23:48 -0700

    Added get-started, identify-criteria, and template.html, made them work, got working input!

--- practice-site.py
+++ practice-site.py
@@ -9,6 +9,23 @@ def home():
     return render_template("homepage.html")
 
 
+@app.route("/get-started")
+def getting_started():
+    """Starts the decision matrix"""
+
+    return render_template("get-started.html")
+
+
+@app.route("/identify-criteria")
+def identifying_criteria():
+    """Walks the user through picking decision criteria"""
+
+    current_user = request.args.get("current-user")
+    user_decision = request.args.get("user-decision")
+
+    return render_template("identify-criteria.html", 
+        name=current_user, 
+        decision=user_decision)
 
 
 
